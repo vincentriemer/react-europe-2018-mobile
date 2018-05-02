9e56ca566eae79afc66a6ecb864f1fd28b49a92d
Patrick Aljord <patcito@gmail.com>
2018-05-03 01:32:12 +0200

    hide scanner on read to fix double scanning on android

--- src/screens/QRScreens/QRScreen.js
+++ src/screens/QRScreens/QRScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   ActivityIndicator,
   AsyncStorage,
@@ -6,31 +6,31 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View,
-} from 'react-native';
-import { BarCodeScanner, Permissions, Notifications } from 'expo';
-import { withNavigation, SafeAreaView } from 'react-navigation';
+  View
+} from "react-native";
+import { BarCodeScanner, Permissions, Notifications } from "expo";
+import { withNavigation, SafeAreaView } from "react-navigation";
 
 @withNavigation
 export default class QRScreen extends React.Component {
   state = {
     showQRScanner: true,
-    hasCameraPermission: null,
+    hasCameraPermission: null
   };
 
   componentDidMount() {
     this._requestCameraPermission();
     this.didBlurSubscription = this.props.navigation.addListener(
-      'didBlur',
+      "didBlur",
       payload => {
-        console.debug('didBlur', payload);
+        console.debug("didBlur", payload);
         this.setState({ showQRScanner: false });
       }
     );
     this.didFocusSubscription = this.props.navigation.addListener(
-      'didFocus',
+      "didFocus",
       payload => {
-        console.debug('didfocus', payload);
+        console.debug("didfocus", payload);
         this.setState({ showQRScanner: true });
       }
     );
@@ -44,47 +44,56 @@ export default class QRScreen extends React.Component {
   _requestCameraPermission = async () => {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({
-      hasCameraPermission: status === 'granted',
+      hasCameraPermission: status === "granted"
+    });
+  };
+
+  _onBarCodeRead = data => {
+    this.setState({ showQRScanner: false }, () => {
+      this.props.onBarCodeRead(data);
     });
   };
 
   render() {
     return (
-      <View style={{ flex: 1, backgroundColor: 'black' }}>
+      <View style={{ flex: 1, backgroundColor: "black" }}>
         {this.state.showQRScanner && this.state.hasCameraPermission ? (
           <BarCodeScanner
-            onBarCodeRead={this.props.loading ? null : this.props.onBarCodeRead}
+            onBarCodeRead={this.props.loading ? null : this._onBarCodeRead}
             style={{ flex: 1 }}
           />
         ) : null}
 
         <SafeAreaView
-          forceInset={{ top: 'always' }}
-          style={{ position: 'absolute', top: 0, left: 0, right: 0 }}>
+          forceInset={{ top: "always" }}
+          style={{ position: "absolute", top: 0, left: 0, right: 0 }}
+        >
           <Text
             style={{
               fontSize: 20,
-              marginTop: Platform.OS === 'ios' ? 15 : 40,
-              textAlign: 'center',
-              color: '#fff',
-            }}>
+              marginTop: Platform.OS === "ios" ? 15 : 40,
+              textAlign: "center",
+              color: "#fff"
+            }}
+          >
             {this.props.title}
           </Text>
         </SafeAreaView>
 
         <SafeAreaView
-          forceInset={{ bottom: 'always' }}
+          forceInset={{ bottom: "always" }}
           style={{
-            position: 'absolute',
+            position: "absolute",
             bottom: 10,
-            paddingBottom: Platform.OS === 'ios' ? 0 : 15,
+            paddingBottom: Platform.OS === "ios" ? 0 : 15,
             left: 0,
             right: 0,
-            alignItems: 'center',
-          }}>
+            alignItems: "center"
+          }}
+        >
           <Button
             onPress={() => this.props.navigation.goBack()}
-            color={Platform.OS === 'ios' ? '#fff' : null}
+            color={Platform.OS === "ios" ? "#fff" : null}
             title="Dismiss"
           />
         </SafeAreaView>
@@ -93,15 +102,16 @@ export default class QRScreen extends React.Component {
             style={[
               StyleSheet.absoluteFill,
               {
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'transparent',
-              },
-            ]}>
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "transparent"
+              }
+            ]}
+          >
             <ActivityIndicator
               color="#fff"
               size="large"
-              style={{ backgroundColor: 'transparent' }}
+              style={{ backgroundColor: "transparent" }}
             />
           </View>
         ) : null}
