3a4121c9859ffb9b2e7e8b16ffe845bf741b834c
Patrick Aljord <patcito@gmail.com>
2018-05-13 16:24:01 +0200

    don't show My tickets button twice

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Animated,
   Linking,
@@ -9,50 +9,50 @@ import {
   ScrollView,
   StyleSheet,
   AsyncStorage,
-  View,
-} from 'react-native';
+  View
+} from "react-native";
 import {
   Asset,
   LinearGradient,
   WebBrowser,
   Video,
   Permissions,
-  Notifications,
-} from 'expo';
-import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
-import { NavigationActions } from 'react-navigation';
-import FadeIn from 'react-native-fade-in-image';
-import { View as AnimatableView } from 'react-native-animatable';
-import { Ionicons } from '@expo/vector-icons';
-import { withNavigation } from 'react-navigation';
+  Notifications
+} from "expo";
+import { BorderlessButton, RectButton } from "react-native-gesture-handler";
+import { NavigationActions } from "react-navigation";
+import FadeIn from "react-native-fade-in-image";
+import { View as AnimatableView } from "react-native-animatable";
+import { Ionicons } from "@expo/vector-icons";
+import { withNavigation } from "react-navigation";
 
-import AnimatedScrollView from '../components/AnimatedScrollView';
-import NavigationBar from '../components/NavigationBar';
-import TalksUpNext from '../components/TalksUpNext';
-import MenuButton from '../components/MenuButton';
-import VideoBackground from '../components/VideoBackground';
-import { BoldText, SemiBoldText } from '../components/StyledText';
-import { connectDrawerButton } from '../Navigation';
-import { Colors, FontSizes, Layout, GQL } from '../constants';
-import { Speakers, Talks } from '../data';
+import AnimatedScrollView from "../components/AnimatedScrollView";
+import NavigationBar from "../components/NavigationBar";
+import TalksUpNext from "../components/TalksUpNext";
+import MenuButton from "../components/MenuButton";
+import VideoBackground from "../components/VideoBackground";
+import { BoldText, SemiBoldText } from "../components/StyledText";
+import { connectDrawerButton } from "../Navigation";
+import { Colors, FontSizes, Layout, GQL } from "../constants";
+import { Speakers, Talks } from "../data";
 import {
   HideWhenConferenceHasStarted,
   HideWhenConferenceHasEnded,
-  ShowWhenConferenceHasEnded,
-} from '../utils';
-export const Schedule = require('../data/schedule.json');
+  ShowWhenConferenceHasEnded
+} from "../utils";
+export const Schedule = require("../data/schedule.json");
 const Event = Schedule.events[0];
 
 class Home extends React.Component {
   state = {
-    scrollY: new Animated.Value(0),
+    scrollY: new Animated.Value(0)
   };
   render() {
     const { scrollY } = this.state;
     const headerOpacity = scrollY.interpolate({
       inputRange: [0, 150],
       outputRange: [0, 1],
-      extrapolate: 'clamp',
+      extrapolate: "clamp"
     });
 
     return (
@@ -64,22 +64,24 @@ class Home extends React.Component {
           onScroll={Animated.event(
             [
               {
-                nativeEvent: { contentOffset: { y: scrollY } },
-              },
+                nativeEvent: { contentOffset: { y: scrollY } }
+              }
             ],
             { useNativeDriver: true }
-          )}>
+          )}
+        >
           <View
             style={{
-              backgroundColor: '#4d5fab',
+              backgroundColor: "#4d5fab",
               padding: 10,
               paddingTop: Layout.headerHeight - 10,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
             <Image
-              source={require('../assets/logo.png')}
-              style={{ width: 220, height: 60, resizeMode: 'contain' }}
+              source={require("../assets/logo.png")}
+              style={{ width: 220, height: 60, resizeMode: "contain" }}
               tintColor="#fff"
             />
             <View style={styles.headerContent}>
@@ -119,22 +121,22 @@ class Home extends React.Component {
   }
 
   _openTickets = () => {
-    Linking.openURL(Event.websiteUrl + '#tickets');
+    Linking.openURL(Event.websiteUrl + "#tickets");
   };
 }
 
 @withNavigation
 class DeferredHomeContent extends React.Component {
   state = {
-    ready: Platform.OS === 'android' ? false : true,
+    ready: Platform.OS === "android" ? false : true,
     hasCameraPermission: null,
     Notification: {},
-    tickets: [],
+    tickets: []
   };
 
   async getTickets() {
     try {
-      const value = await AsyncStorage.getItem('@MySuperStore:tickets');
+      const value = await AsyncStorage.getItem("@MySuperStore:tickets");
       // console.log("tickets", value);
       this.setState({ tickets: JSON.parse(value) });
       this.tickets = JSON.parse(value);
@@ -186,32 +188,21 @@ class DeferredHomeContent extends React.Component {
             <RectButton
               style={styles.bigButton}
               onPress={this._handlePressStaffCheckinListsButton}
-              underlayColor="#fff">
+              underlayColor="#fff"
+            >
               <SemiBoldText style={styles.bigButtonText}>
                 Go to checkin
               </SemiBoldText>
             </RectButton>
           </ClipBorderRadius>
         ) : null}
-        {tix && tix.length > 0 ? (
-          <ClipBorderRadius>
-            <RectButton
-              style={styles.bigButton}
-              onPress={() => this.props.navigation.navigate('Profile')}
-              underlayColor="#fff">
-              <SemiBoldText style={styles.bigButtonText}>
-                My Tickets
-              </SemiBoldText>
-            </RectButton>
-          </ClipBorderRadius>
-        ) : null}
-
         {tix && tix.length === 0 ? (
           <ClipBorderRadius>
             <RectButton
               style={styles.bigButton}
               onPress={this._handlePressQRButton}
-              underlayColor="#fff">
+              underlayColor="#fff"
+            >
               <SemiBoldText style={styles.bigButtonText}>
                 Scan your conference ticket QR code
               </SemiBoldText>
@@ -222,8 +213,9 @@ class DeferredHomeContent extends React.Component {
           <ClipBorderRadius>
             <RectButton
               style={styles.bigButton}
-              onPress={() => this.props.navigation.navigate('Profile')}
-              underlayColor="#fff">
+              onPress={() => this.props.navigation.navigate("Profile")}
+              underlayColor="#fff"
+            >
               <SemiBoldText style={styles.bigButtonText}>
                 My tickets
               </SemiBoldText>
@@ -245,8 +237,9 @@ class DeferredHomeContent extends React.Component {
           <ClipBorderRadius>
             <RectButton
               style={styles.bigButton}
-              onPress={() => this.props.navigation.navigate('Profile')}
-              underlayColor="#fff">
+              onPress={() => this.props.navigation.navigate("Profile")}
+              underlayColor="#fff"
+            >
               <SemiBoldText style={styles.bigButtonText}>
                 My Tickets
               </SemiBoldText>
@@ -258,7 +251,8 @@ class DeferredHomeContent extends React.Component {
             <RectButton
               style={styles.bigButton}
               onPress={this._handlePressQRButton}
-              underlayColor="#fff">
+              underlayColor="#fff"
+            >
               <SemiBoldText style={styles.bigButtonText}>
                 Scan your conference ticket QR code
               </SemiBoldText>
@@ -270,7 +264,8 @@ class DeferredHomeContent extends React.Component {
             <RectButton
               style={styles.bigButton}
               onPress={this._handlePressQRButton}
-              underlayColor="#fff">
+              underlayColor="#fff"
+            >
               <SemiBoldText style={styles.bigButtonText}>
                 Scan another ticket QR code
               </SemiBoldText>
@@ -281,7 +276,8 @@ class DeferredHomeContent extends React.Component {
           <RectButton
             style={styles.bigButton}
             onPress={this._handlePressCOCButton}
-            underlayColor="#fff">
+            underlayColor="#fff"
+          >
             <SemiBoldText style={styles.bigButtonText}>
               Read the code of conduct
             </SemiBoldText>
@@ -292,9 +288,10 @@ class DeferredHomeContent extends React.Component {
           <RectButton
             style={styles.bigButton}
             onPress={this._handlePressMapButton}
-            underlayColor="#fff">
+            underlayColor="#fff"
+          >
             <SemiBoldText style={styles.bigButtonText}>
-              {Platform.OS === 'android' ? 'Download' : 'Open'} the conference
+              {Platform.OS === "android" ? "Download" : "Open"} the conference
               map
             </SemiBoldText>
           </RectButton>
@@ -304,15 +301,16 @@ class DeferredHomeContent extends React.Component {
           <RectButton
             style={styles.bigButton}
             onPress={this._handlePressTwitterButton}
-            underlayColor="#fff">
+            underlayColor="#fff"
+          >
             <Ionicons
               name="logo-twitter"
               size={23}
               style={{
-                color: '#fff',
+                color: "#fff",
                 marginTop: 3,
-                backgroundColor: 'transparent',
-                marginRight: 5,
+                backgroundColor: "transparent",
+                marginRight: 5
               }}
             />
             <SemiBoldText style={styles.bigButtonText}>
@@ -327,7 +325,7 @@ class DeferredHomeContent extends React.Component {
   _handlePressAllTalks = () => {
     this.props.navigation.dispatch(
       NavigationActions.navigate({
-        routeName: 'Schedule',
+        routeName: "Schedule"
       })
     );
   };
@@ -338,14 +336,14 @@ class DeferredHomeContent extends React.Component {
 
   _handlePressQRButton = () => {
     this.props.navigation.navigate({
-      routeName: 'QRScanner',
-      key: 'QRScanner',
+      routeName: "QRScanner",
+      key: "QRScanner"
     });
   };
 
   _handlePressStaffCheckinListsButton = () => {
     // console.log("handle press checkinlists");
-    this.props.navigation.navigate('StaffCheckinLists');
+    this.props.navigation.navigate("StaffCheckinLists");
   };
 
   _handlePressTwitterButton = async () => {
@@ -354,27 +352,27 @@ class DeferredHomeContent extends React.Component {
         `twitter://user?screen_name=` + Event.twitterHandle
       );
     } catch (e) {
-      WebBrowser.openBrowserAsync('https://twitter.com/' + Event.twitterHandle);
+      WebBrowser.openBrowserAsync("https://twitter.com/" + Event.twitterHandle);
     }
   };
 
   _handlePressMapButton = () => {
     const params = encodeURIComponent(
-      Event.venueName + Event.venueCity + ',' + Event.venueCountry
+      Event.venueName + Event.venueCity + "," + Event.venueCountry
     );
-    WebBrowser.openBrowserAsync('https://www.google.com/maps/search/' + params);
+    WebBrowser.openBrowserAsync("https://www.google.com/maps/search/" + params);
   };
 }
 
 const OverscrollView = () => (
   <View
     style={{
-      position: 'absolute',
+      position: "absolute",
       top: -400,
       height: 400,
       left: 0,
       right: 0,
-      backgroundColor: Colors.blue,
+      backgroundColor: Colors.blue
     }}
   />
 );
@@ -383,9 +381,10 @@ const ClipBorderRadius = ({ children, style }) => {
   return (
     <View
       style={[
-        { borderRadius: BORDER_RADIUS, overflow: 'hidden', marginTop: 10 },
-        style,
-      ]}>
+        { borderRadius: BORDER_RADIUS, overflow: "hidden", marginTop: 10 },
+        style
+      ]}
+    >
       {children}
     </View>
   );
@@ -395,50 +394,50 @@ const BORDER_RADIUS = 3;
 
 const styles = StyleSheet.create({
   headerContent: {
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 5,
-    paddingVertical: 10,
+    paddingVertical: 10
   },
   headerVideoLayer: {
-    ...StyleSheet.absoluteFillObject,
+    ...StyleSheet.absoluteFillObject
   },
   headerVideoOverlay: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: Colors.blue,
-    opacity: 0.8,
+    opacity: 0.8
   },
   headerText: {
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
     fontSize: 17,
-    lineHeight: 17 * 1.5,
+    lineHeight: 17 * 1.5
   },
   headerSmallText: {
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center",
     fontSize: 7,
-    lineHeight: 7 * 1.5,
+    lineHeight: 7 * 1.5
   },
   bigButton: {
     backgroundColor: Colors.blue,
     paddingHorizontal: 15,
     height: 50,
     marginHorizontal: 15,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     borderRadius: BORDER_RADIUS,
-    overflow: 'hidden',
-    flexDirection: 'row',
+    overflow: "hidden",
+    flexDirection: "row"
   },
   bigButtonText: {
     fontSize: FontSizes.normalButton,
-    color: '#fff',
-    textAlign: 'center',
+    color: "#fff",
+    textAlign: "center"
   },
   seeAllTalks: {
     fontSize: FontSizes.normalButton,
-    color: Colors.blue,
-  },
+    color: Colors.blue
+  }
 });
 
 export default Home;
